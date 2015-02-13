7e1250c04f41b413513a0879713c5294b2a441a3
Sean Rowan <sean.rowan15@gmail.com>
2015-02-12 18:41:07 -0800

    fixing link

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ if ('development' == app.get('env')) {
 app.get('/', index.view);
 app.get('/admin_enter_wine', admin_enter_wine.view);
 app.get('/log_all_wine_json',log_all_wine_json.all_wine);
-//app.get('/pinot_holder', pinot_holder.view)
+app.get('/pinot_holder', pinot_holder.view)
 
 //app.get('/add', add.addFriend);
 // Example route
