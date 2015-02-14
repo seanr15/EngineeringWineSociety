2945ee00b6b1ce90f7419df9d01b68045272d57f
vagrant <vagrant@precise32.(none)>
2015-02-14 21:50:08 +0000

    ...

---
+++ routes/admin_delete_wine.js
@@ -0,0 +1,27 @@
+var pg = require('pg');
+
+
+exports.delete_wine = function(req, res){
+  console.log("Wine to delete from database");
+  console.log(req.query.wine);
+    pg.connect(process.env.DATABASE_URL, 
+	           function(err, client) {
+               var query = client.query('Delete From AllWines Where name=$1 RETURNING name',[req.query.wine],
+	                                       function(err,result){
+                                           if(err){
+                                             console.log(err);
+															             }
+															             else {
+                                             console.log('row Deleted with name: ' + result.rows[0].name);
+                                           }
+															             client.end();
+													               });
+														 
+
+    
+             });
+  	res.render('index');
+
+	
+   
+};
