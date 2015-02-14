f05f1f6000d57ffa310f883769d78756af42979b
vagrant <vagrant@precise32.(none)>
2015-02-14 20:21:38 +0000

    ...

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,11 @@ exports.view = function(req, res){
    pg.connect(process.env.DATABASE_URL, function(err, client) {
    var query = client.query('SELECT * FROM AllWines');
     
-
+    var i = 0;
     query.on('row', function(row,result) {
       console.log(JSON.stringify(row));
-		  allwines['allwines'].push(row);
+		  allwines['allwines'].push(row.push({'id':i++});
+			
     });
 	  query.on('end',function(result){
 	    console.log(allwines);
