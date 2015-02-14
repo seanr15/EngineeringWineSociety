a3a608bed4c1d599004ad3471ce25c09dd6462b2
vagrant <vagrant@precise32.(none)>
2015-02-14 19:59:14 +0000

    working on row selection for pinot_holder

--- routes/pinot_holder.js
+++ routes/pinot_holder.js
@@ -1,6 +1,11 @@
 
 exports.view = function(req, res){
 
+  console.log("New Wine Data to View");
+  console.log(req.query.url);
+  console.log(req.query.wine);
+  console.log(req.query.varietal);
+  console.log(req.query.vineyard);
   res.render('pinot_holder');
   
   
