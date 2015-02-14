3adc57d0837df66543ac69f10a12fb92195ac700
vagrant <vagrant@precise32.(none)>
2015-02-14 21:03:17 +0000

    finishing dynamic pinot_handlebar

--- routes/pinot_holder.js
+++ routes/pinot_holder.js
@@ -3,11 +3,8 @@ var pg = require('pg');
 exports.view = function(req, res){
 
   console.log("New Wine Data to View");
-  console.log(req.query.url);
   console.log(req.query.wine);
-  console.log(req.query.varietal);
-  console.log(req.query.vineyard);
-
+  
 
 
 
