b86dcc404cf63c60a5712fc2a95e5311d4178ce6
vagrant <vagrant@precise32.(none)>
2015-02-15 01:23:42 +0000

    ...

--- routes/process_login.js
+++ routes/process_login.js
@@ -1,10 +1,43 @@
+var pg = require('pg');
+var bcrypt = require('bcrypt');
+
+
 exports.process = function(req, res){
     console.log('Log in info to look up');
     
 
 		
-    console.log(req.query.email);
+    console.log(req.query.username);
     console.log(req.query.pass);
+    console.log(req.query.newuser);
+		
+	 /*	
+   var thiswine={'thiswine':[]};
+   pg.connect(process.env.DATABASE_URL, function(err, client) {
+   var query = client.query('SELECT * FROM USERS Where username=$1 AND phash=crypt($1, phash)',[req.query.username,req.query.pass]);
+    
+    query.on('row', function(row,result) {
+      console.log(JSON.stringify(row));
+		  thiswine['thiswine'].push(row);
+			
+    });
+	  query.on('end',function(result){
+	    console.log(thiswine);
+			//render here for data
+			var i = 0;
+			for (var wine_row in thiswine['thiswine']) {
+        thiswine['thiswine'][wine_row]['id']=i;
+				i = i + 1;
+
+      }
+    	res.render('pinot_holder',thiswine);
+			
+		  client.end();
+
+	  });					 
+  });
+
+*/
   
 		res.redirect('index');
 };
