0ccce83978c58604d5b4f5b0cef3a3236ecae1a9
vagrant <vagrant@precise32.(none)>
2015-02-15 04:43:00 +0000

    ...

--- routes/process_login.js
+++ routes/process_login.js
@@ -14,8 +14,87 @@ exports.process = function(req, res){
     console.log(req.body.email);
     console.log(req.body.password);
     console.log(req.body.newuser);
+		if( req.body.newuser == 'sign_up' ){
+      bcrypt.genSalt(10, function(err, salt) {
+                  bcrypt.hash(req.body.password, salt, function(err, hash) {
+                        // Store hash in your password DB.
+                               pg.connect(process.env.DATABASE_URL, 
+	                                       function(err, client) {
+                                           var query = client.query('Insert Into Users values($1,$2,$3) RETURNING username',
+																				                           [req.body.email,hash,'A'],
+	                                                                 function(err,result){
+                                                                      if(err){
+                                                                        console.log(err);
+															                                        }
+															                                     else {
+                                                                         console.log('row inserted with id: ' + result.rows[0].username);
+                                                                     }
+		                                                                  res.redirect('index');
+																																		 
+															                                       client.end();
+													                                         });
+														 
+
+    
+                              });
+                         
+
+
+                 });
+      });
+
+
+
+		}
+		else{
+
+
+		 var user={'user':[]};
+     pg.connect(process.env.DATABASE_URL, function(err, client) {
+     var query = client.query('SELECT * FROM Users where username=$1',[req.body.email]);
+    
+    
+    query.on('row', function(row,result) {
+      console.log(JSON.stringify(row));
+		  user['user'].push(row);
+			
+    });
+	  query.on('end',function(result){
+	    console.log(user['user'][0]);
+			//render here for data
+		  bcrypt.compare( req.body.password,user['user'][0]['phash'], function(err, res) {
+       
+			 if(res === true){
+			 res.redirect('index');
+			   
+
+
+			 }
+			 res.redirect('login');
+			 
+
+    
+		
+		
+		
+		});
+			
+			
+		  client.end();
+
+	  });					 
+  });
+
+		
+
+
+
+
+
+
+		}
 		
   
-		res.redirect('index');
+		//res.redirect('index');
 };
 
