b08e72884503afe5104e5cbdfd326026b5eb67f4
vagrant <vagrant@precise32.(none)>
2015-02-18 23:20:25 +0000

    ...

--- public/js/EWSModal.js
+++ public/js/EWSModal.js
@@ -4,22 +4,22 @@ $(document).ready(function() {
   console.log("ready to run modal");
   $('#exampleModal').on('show.bs.modal', function (event) {
   var button = $(event.relatedTarget); // Button that triggered the modal
-  var name1 = button.data('name'); // Extract info from data-* attributes
-	var winery = button.data('winery');
-	var varietal = button.data('varietal');
-	console.log("try 1" + name1)
+  //var name1 = button.data('name'); // Extract info from data-* attributes
+	//var winery = button.data('winery');
+	//var varietal = button.data('varietal');
+	//console.log("try 1" + name1)
   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
   // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-  var modal = $(this);
+  /*var modal = $(this);
   modal.find('.modal-title').text('Add Wine');
-  var name2 =  modal.find('.modal-body input').val(name);
-	console.log("try 2" + name2);
+  var form =  modal.find('.modal-body form').val(add_wine);
+	console.log("try 2" + form);*/
 
 	   
 		 ///adding click to primary button '#add'
 	   /*$('#add').click(//callback
 		   function(){
-			   $('#add_wine').submit();
+			   form.submit();
      });*/
 		 /////
 
