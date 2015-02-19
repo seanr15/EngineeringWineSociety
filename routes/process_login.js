895ff1fe9f38b1902eadf56c92316389c6ec5b72
vagrant <vagrant@precise32.(none)>
2015-02-19 21:01:43 +0000

    ...

--- routes/process_login.js
+++ routes/process_login.js
@@ -43,50 +43,60 @@ exports.process = function(req, res){
 			}
 			else{
 			//render here for data
-		    bcrypt.compare( req.body.password,user['user'][0]['phash'], 
+		  bcrypt.compare( req.body.password,user['user'][0]['phash'], 
 			                function(err, result2) {
        
 			                  if(result2 === true){
-
-			 	                  client.end();
+                          console.log("password Valid!");
 													//determine if user is admin
 			                    if(user['user'][0]['role']=='A'){
+			 	                    client.end();
+													 
 		                        res.redirect('index');
-												  }
-												  else{
-
-		                         var allwines={'allwines':[]};
-
-                            var query2 = client.query('SELECT * FROM userratings where username=$1',[req.body.email]);
-                            query2.on('row', function(row,result) {
-		                          allwines['allwines'].push(row);
-					
-                            });
-	                           query2.on('end',function(err,result){
-	    			                  
-		                           if( result.rows.length == 0 ){
-															    console.log('NO rated Wines !!!!!!!!!!!');
-															   
-                                 client.end();
-					                       res.render('user_index');
-         
-			                         }
-														   else{
-															    console.log('All rated Wines !!!!!!!!!!!');
-														      client.end();
-                                 res.render('user_index',allwines);
-															}
-													 });
+													}
+													else{
+                            console.log("Valid User!");
+ 
+													}
+												/*	else{
+		                        //res.redirect('/user_index');
+														
+													}*/
 					              }
-			                  
-									  }
-										else{
+			                  else{
 		                      client.end();
 		                      res.redirect('/');
-										}
+												}
 			  });
 			}
-		});					 
+		});
+  });
+
+
+	 var allwines={'allwines':[]};
+   pg.connect(process.env.DATABASE_URL, function(err, client) {
+   var query = client.query('SELECT * FROM userratings where username=$1'[req.body.email]);
+    
+    
+    query.on('row', function(row,result) {
+      console.log(JSON.stringify(row));
+		  allwines['allwines'].push(row);
+			
+    });
+	  query.on('end',function(result){
+	    console.log(allwines);
+			//render here for data
+			var i = 0;
+			for (var wine_row in allwines['allwines']) {
+        allwines['allwines'][wine_row]['id']=i;
+				i = i + 1;
+
+      }
+    	res.render('user_index',allwines);
+			
+		  client.end();
+
+	  });					 
   });
 
 //	}
