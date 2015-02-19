b9091351ee59d9614b8e17f59c548a0c703a691e
vagrant <vagrant@precise32.(none)>
2015-02-19 01:44:16 +0000

    ...

--- routes/admin_sign_up_check.js
+++ routes/admin_sign_up_check.js
@@ -2,8 +2,8 @@ exports.check = function(req, res){
     console.log('new user to add to table'); 
     console.log(req.body.firstname);     
     console.log(req.body.lastname);     
-    console.log(req.body.username);     
+    console.log(req.body.email);     
     console.log(req.body.password);   
 		
-    res.redirect('admin_sign_up_succcess');
+    res.redirect('admin_sign_up_success');
 };
