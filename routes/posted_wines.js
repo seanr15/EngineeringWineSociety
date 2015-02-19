eca14c38a7b3bacfe0ba203f47abf759ae64b8a9
vagrant <vagrant@precise32.(none)>
2015-02-19 23:12:36 +0000

    ...

---
+++ routes/posted_wines.js
@@ -0,0 +1,31 @@
+var pg = require('pg');
+
+exports.view = function(req, res){
+
+   var allwines={'allwines':[]};
+   pg.connect(process.env.DATABASE_URL, function(err, client) {
+   var query = client.query('SELECT * FROM postedwines');
+    
+    
+    query.on('row', function(row,result) {
+      console.log(JSON.stringify(row));
+		  allwines['allwines'].push(row);
+			
+    });
+	  query.on('end',function(result){
+	    console.log(allwines);
+			//render here for data
+			var i = 0;
+			for (var wine_row in allwines['allwines']) {
+        allwines['allwines'][wine_row]['id']=i;
+				i = i + 1;
+
+      }
+    	res.render('posted_wines',allwines);
+			
+		  client.end();
+
+	  });					 
+ });
+  
+};
