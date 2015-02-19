0c9a19e3ecd006b1f486815a0bacc87cf8a46727
vagrant <vagrant@precise32.(none)>
2015-02-19 21:16:22 +0000

    ...

--- routes/process_login.js
+++ routes/process_login.js
@@ -55,6 +55,33 @@ exports.process = function(req, res){
 													}
 													else{
                             console.log("Valid User!");
+														
+														var allwines={'allwines':[]};
+                            var query2 = client.query('SELECT * FROM userratings where username=$1',[user['user'][0]['username']],
+														  function(err,result){
+														  
+															  if(result.rows.length==0){
+                                  client.end();
+					                        res.redirect('/');
+																}
+																else{
+                                allwines['allwines']=result.rows;
+																client.end();
+													      
+                           	    res.render('user_index',allwines);
+
+																}
+														
+														
+														
+														
+														  }
+														);
+    
+    		                     
+			
+
+	                         });					 
  
 													}
 												/*	else{
@@ -72,31 +99,7 @@ exports.process = function(req, res){
   });
 
 
-	 var allwines={'allwines':[]};
-   pg.connect(process.env.DATABASE_URL, function(err, client) {
-   var query = client.query('SELECT * FROM userratings where username=$1',[req.body.email]);
-    
-    
-    query.on('row', function(row,result) {
-      console.log(JSON.stringify(row));
-		  allwines['allwines'].push(row);
-			
-    });
-	  query.on('end',function(result){
-	    console.log(allwines);
-			//render here for data
-			var i = 0;
-			for (var wine_row in allwines['allwines']) {
-        allwines['allwines'][wine_row]['id']=i;
-				i = i + 1;
-
-      }
-    	res.render('user_index',allwines);
-			
-		  client.end();
-
-	  });					 
-  });
+	 
 
 //	}
 		
